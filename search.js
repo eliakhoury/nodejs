861f16b185f519105572c051bef2ab089e0ef880
eliakhoury <45151863+eliakhoury@users.noreply.github.com>
2018-11-19 12:32:50 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,12 @@
+//file system module allows to work with the file system on computer.
 var fs = require('fs');
+//
 var path = require('path');
 
 var fileType = '.' + process.argv[2];
 var subName = process.argv[3];
 
-var flag = 0;
-
-if(process.argv.length < 3){
+if(process.argv.length != 4 ){
   console.log('USAGE: node search [EXT] [TEXT]');
   process.exit(1);
 }
@@ -21,18 +21,13 @@ fs.readdir('./', (err, files)=>{
   }
 })
 
-
 function getFiles(cDir) {
   fs.readdir(cDir, (err, files) => {
     if (err) return console.error(err);
     for (var i = 0; i < files.length; i++) {
       if(path.extname(files[i])===fileType && (files[i].indexOf(subName) > -1)){
-        flag = 1;
-        console.log(files[i]);
+        console.log(__dirname + '\\'+ (cDir.substring(2).length>0 ? cDir.substring(2) + '\\' : '') + files[i]);
       }
-    }
-    if(flag==0){
-      console.log('No file was found');
     }
   })
 }
